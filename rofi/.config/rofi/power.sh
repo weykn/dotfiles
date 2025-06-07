aa19814405b9eee0d1aeaa1d1a63e5713ce07294
weykn <samistuder0@gmail.com>
2025-06-07 09:21:45 +0200

    Fix Powermenu

--- rofi/.config/rofi/power.sh
+++ rofi/.config/rofi/power.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# Options
-shutdown="󰐥\tPoweroff"
-reboot="\tReboot"
-lock="\tLock"
-suspend="󰤄\tSleep"
+# Define labels (with icons)
+shutdown="󰐥  Poweroff"
+reboot="  Reboot"
+lock="  Lock"
+suspend="󰤄  Sleep"
 
+# Show the menu and get the selection
 chosen=$(echo -e "$lock\n$suspend\n$reboot\n$shutdown" | rofi -dmenu -i -p "power")
 
+# Match based on keywords only
 case "$chosen" in
-    "$shutdown") systemctl poweroff ;;
-    "$reboot") systemctl reboot ;;
-    "$lock") i3lock -c 000000  ;;  # Or i3lock-color / betterlockscreen
-    "$suspend") systemctl suspend ;;
+    *Poweroff*) systemctl poweroff ;;
+    *Reboot*) systemctl reboot ;;
+    *Lock*) i3lock -c 000000 ;;
+    *Sleep*) systemctl suspend ;;
     *) exit 1 ;;
 esac
