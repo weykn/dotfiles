30a4430dc3a0a733c4a9bde1a0d1a082704eddbf
weykn <samistuder0@gmail.com>
2025-06-08 23:52:34 +0200

    Added Fish Functions

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[![Watch the demo](https://img.youtube.com/vi/zTJ2DGjOS0Y/0.jpg)](https://www.youtube.com/watch?v=zTJ2DGjOS0Y)
+![ss](./ss.png)
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,2 +1,32 @@
+
+# Startup
 oh-my-posh init fish --config $HOME/.config/fish/zash.omp.json | source
 afetch
+
+# Keybinds
+bind \cf fcd
+bind \ch fhist
+bind \ck 'clear; commandline -f repaint'
+
+# Functions
+
+# Fuzzy History
+function fhist
+    set cmd (history | fzf --tac)
+    if test -n "$cmd"
+        eval $cmd
+    end
+end
+
+# Fuzzy CD
+function fcd
+    set choice (fd . | fzf)
+    if test -z "$choice"
+        return
+    end
+    if test -d "$choice"
+        cd "$choice"
+    else
+        cd (dirname "$choice")
+    end
+end
