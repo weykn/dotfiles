e1c3b92934600648a8990058c3186af9c886d8b0
weykn <samistuder0@gmail.com>
2026-05-08 19:24:04 +0200

    updates

--- rofi/.config/rofi/power.sh
+++ rofi/.config/rofi/power.sh
@@ -4,16 +4,14 @@
 shutdown="󰐥  Poweroff"
 reboot="  Reboot"
 lock="  Lock"
-suspend="󰤄  Sleep"
 
 # Show the menu and get the selection
-chosen=$(echo -e "$lock\n$suspend\n$reboot\n$shutdown" | rofi -dmenu -i -p "power")
+chosen=$(echo -e "$lock\n$reboot\n$shutdown" | rofi -dmenu -i -p "power")
 
 # Match based on keywords only
 case "$chosen" in
-    *Poweroff*) systemctl poweroff ;;
-    *Reboot*) systemctl reboot ;;
-    *Lock*) i3lock -c 000000 ;;
-    *Sleep*) systemctl suspend ;;
-    *) exit 1 ;;
+*Poweroff*) poweroff ;;
+*Reboot*) reboot ;;
+*Lock*) i3lock -c 000000 ;;
+*) exit 1 ;;
 esac
